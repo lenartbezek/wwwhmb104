9cccca1d1d1439ed32d4f4dfd5717bd857cec6c3
Lenart Bezek <lenart.bezek@erpium.com>
2019-12-10 11:19:18 +0100

    Capture errors.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer, ValidationError } from "apollo-server-express";
 import { GraphQLSchema } from "graphql";
 import { Db } from "mongodb";
+import { apm } from "./apm";
 import { Context } from "./context";
 
 export function buildServer(schema: GraphQLSchema, dependencies: {
@@ -12,5 +13,19 @@ export function buildServer(schema: GraphQLSchema, dependencies: {
         schema,
         playground: true,
         debug: false,
+        formatError: (error) => {
+            if (
+                error instanceof ValidationError ||
+                error.originalError instanceof ValidationError
+            ) {
+                return error;
+            }
+            apm.captureError(error.originalError ?? error, {
+                labels: {
+                    path: error.path?.join("."),
+                },
+            });
+            return error;
+        },
     });
 }
--- src/start.ts
+++ src/start.ts
@@ -1,4 +1,4 @@
-import "./apm";
+import { apm } from "./apm";
 
 import * as express from "express";
 import { connectToDb } from "./mongo";
@@ -13,6 +13,25 @@ async function start() {
     const server = buildServer(schema, { db });
 
     server.applyMiddleware({ app });
+
+    app.get("/something", (req, res, next) => {
+        throw new Error("Something happened.");
+    });
+
+    app.use(async (error: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+        const id = await new Promise<string>((resolve, reject) => apm.captureError(error, (err, errId) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(errId);
+            }
+        }));
+        if (res.headersSent) {
+            next(error);
+        } else {
+            res.status(500).send(`INTERNAL_SERVER_ERROR ${id}`);
+        }
+    });
 
     app.listen({ port: 8080 }, () => {
         // tslint:disable-next-line: no-console
